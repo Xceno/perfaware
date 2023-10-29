77ca895784e5b69453fb20f5347a5c56ace7cf14
Xceno <rob@xceno.io>
2023-10-29 18:27:09 +0100

    Add 8086 manual and example files for part1.

---
+++ resources/part1/listing_0038_many_register_mov-REBUILT-REBUILT.asm
@@ -0,0 +1,15 @@
+; Original-File: ..\..\resources\part1\listing_0038_many_register_mov-REBUILT
+
+bits 16
+
+MOV CX, BX
+MOV CH, AH
+MOV DX, BX
+MOV SI, BX
+MOV BX, DI
+MOV AL, CL
+MOV CH, CH
+MOV BX, AX
+MOV BX, SI
+MOV SP, DI
+MOV BP, AX
