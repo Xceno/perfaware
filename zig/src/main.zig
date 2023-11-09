51abd6af930d7af288b4979b69f1ac7cdceaf318
Xceno <rob@xceno.io>
2023-11-09 11:53:05 +0100

    Add haversine read + calcs in zig

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -6,11 +6,14 @@
 //!
 
 const std = @import("std");
+const math = std.math;
 const testing = std.testing;
 
 const RndGen = std.rand.DefaultPrng;
 const Allocator = std.mem.Allocator;
 var prng = RndGen.init(0);
+
+const earth_radius = 6371;
 
 fn rand_latitude() f32 {
     return -90.0 + (180.0 * prng.random().float(f32));
@@ -25,6 +28,14 @@ const Coordinates = struct {
     y0: f32,
     x1: f32,
     y1: f32,
+};
+
+const HaversineResult = struct {
+    result: f64,
+    time_input: u64,
+    time_math: u64,
+    time_total: u64,
+    throughput: []const u8,
 };
 
 fn create_example_data(allocator: Allocator, num_entries: u64) ![]Coordinates {
@@ -64,35 +75,82 @@ fn write_example_json(allocator: Allocator, num_entries: u64, output_path: []con
     const data = try create_example_data(allocator, num_entries);
     defer allocator.free(data);
 
-    //try stdout.print("Wrote {d} items", .{data.length()});
+    std.debug.print("Wrote {d} items", .{data.len});
 
     try write_data_to_json(output_path, data);
 }
 
-fn nanos_to_seconds(nanos: i128) f128 {
-    const result = nanos / 1000000000;
+fn haversine_of_degrees(coords: Coordinates, r: f32) f32 {
+    const dy = math.degreesToRadians(f32, coords.y1 - coords.y0);
+    const dx = math.degreesToRadians(f32, coords.x1 - coords.x0);
+    const y0 = math.degreesToRadians(f32, coords.y0);
+    const y1 = math.degreesToRadians(f32, coords.y1);
+
+    const root_term = (math.pow(f32, math.sin(dy / 2.0), 2.0) + (math.cos(y0) * math.cos(y1) * math.pow(f32, math.sin(dx / 2.0), 2.0)));
+    const result = (2 * r * math.asin(math.sqrt(root_term)));
+
     return result;
 }
 
+fn read_haversine_json(allocator: Allocator, path: []const u8) !std.json.Parsed([]Coordinates) {
+    const file = try std.fs.cwd().openFile(path, .{});
+    const file_size = (try file.stat()).size;
+    var buffer = try allocator.alloc(u8, file_size);
+    try file.reader().readNoEof(buffer);
+    var parsed_result = try std.json.parseFromSlice([]Coordinates, allocator, buffer, .{});
+
+    return parsed_result;
+}
+
+fn run_haversine() !HaversineResult {
+    var json_input_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer json_input_arena.deinit();
+
+    std.log.debug("reading haversine-data.json", .{});
+    var timer = try std.time.Timer.start();
+    const parsed = try read_haversine_json(json_input_arena.allocator(), "haversine-data.json");
+    const haversine_data = parsed.value;
+    const input_time = timer.read();
+    std.log.debug("done reading.", .{});
+
+    std.log.debug("Calculating average", .{});
+    timer.reset();
+    var sum: f32 = 0.0;
+    var cnt: i32 = 0;
+
+    for (haversine_data) |coords| {
+        sum += haversine_of_degrees(coords, earth_radius);
+        cnt += 1;
+    }
+
+    const average_distance: f64 = (sum / @as(f32, @floatFromInt(cnt)));
+    const math_time = timer.read();
+
+    return .{
+        .result = average_distance,
+        .time_input = input_time,
+        .time_math = math_time,
+        .time_total = input_time + math_time,
+        .throughput = "TODO",
+    };
+}
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // var example_data_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    // defer example_data_arena.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    // const stdout_file = std.io.getStdOut().writer();
-    // var bw = std.io.bufferedWriter(stdout_file);
-    // const stdout = bw.writer();
+    // const count = 10000000;
+    // const out_path = "haversine-data.json";
+    // try write_example_json(example_data_arena.allocator(), count, out_path);
 
-    var example_data_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer example_data_arena.deinit();
-
-    const count = 5;
-    const out_path = "haversine-data.json";
-    try write_example_json(example_data_arena.allocator(), count, out_path);
-
-    // try bw.flush(); // don't forget to flush!
+    const hav_result = try run_haversine();
+    std.log.info("RESULT: {d}\nINPUT: {any}\nMATH: {any}\nTOTAL: {any}\nTHROUGH: {s}", .{
+        hav_result.result,
+        std.fmt.fmtDuration(hav_result.time_input),
+        std.fmt.fmtDuration(hav_result.time_math),
+        std.fmt.fmtDuration(hav_result.time_total),
+        hav_result.throughput,
+    });
 }
 
 test "simple test" {
@@ -105,7 +163,16 @@ test "simple test" {
 test "Creating example json should not leak memory" {
     const test_file = "tmp-testing.json";
     defer {
-        std.fs.cwd().deleteFile(test_file) catch unreachable;
+        std.fs.cwd().deleteFile(test_file) catch |err| {
+            switch (err) {
+                error.FileNotFound => {
+                    std.log.err("Temp output file doesn't exist", .{});
+                },
+                else => {
+                    std.log.err("Couldn't delete tmp file: {}", .{err});
+                },
+            }
+        };
     }
 
     try write_example_json(testing.allocator, 10, test_file);
